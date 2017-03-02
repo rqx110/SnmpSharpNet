9dae1c389797d48ab4284f2733201fea966ece1e
qingxiao_ren <qingxiao_ren@ao-inc.com.cn>
2017-03-02 08:56:52 +0800

    upgrade version to 0.9.5

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("33f5795a-9f55-4965-89bc-0f413452bd0f")]
 
-[assembly: AssemblyVersion("0.9.4")]
-[assembly: AssemblyFileVersion("0.9.4")]
+[assembly: AssemblyVersion("0.9.5")]
+[assembly: AssemblyFileVersion("0.9.5")]
